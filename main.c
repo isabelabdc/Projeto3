0b53eb52a12a2cc27a26274cceaaaf6fbd8cfa08
isabelabdc <ibonoto@yahoo.com>
2025-05-11 19:50:18 -0300

    Posicionando 3 navios aleatoriamente

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>  //biblioteca para sortear numeros aleatorios;
 
 //tabuleiro inicial:
 void iniciarTabuleiro(int tabuleiro[5][5]){ 
@@ -29,5 +30,19 @@ void exibirTabuleiro(int tabuleiro[5][5]){
             }
         }
         printf("\n");
+    }
+}
+
+//sorteia 3 navios aleatoriamente:
+void posicionarNavios(int tabuleiro[5][5]){
+    srand(time(NULL));
+    int navios=0;
+    while (navios<3){
+        int x = rand()%5;  //sorteia o x;
+        int y= rand()%5;  //sorteia o y;
+        if(tabuleiro[x][y]==1){  //apenas marca a posicao se a mesma estiver livre (se for agua);
+            tabuleiro[x][y]=9;  // "9" representa um navio oculto;
+            navios++;
+        }
     }
 }
