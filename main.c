c5ff0a4cc020cb5c99329814c01189891bd6214c
isabelabdc <ibonoto@yahoo.com>
2025-05-11 19:48:25 -0300

    Função que imprime o tabuleiro

--- main.c
+++ main.c
@@ -9,3 +9,25 @@ void iniciarTabuleiro(int tabuleiro[5][5]){
         }
     }
 }
+
+//imprimir o tabuleiro:
+void exibirTabuleiro(int tabuleiro[5][5]){ 
+    printf("\n\t");
+    for (int i=0; i<5; i++){
+        printf("\t%d", i+1);  //imprime coordenadas;
+    }
+    printf("\n");
+    for (int i=0; i<5; i++){
+        printf("\t%d", i+1);  //imprime coordenadas; 
+        for(int j=0; j<5; j++){
+            if(tabuleiro[i][j]==1 || tabuleiro[i][j]==9){
+                printf("\t~"); //agua;
+            }else if(tabuleiro[i][j]==2){
+                printf("\tX"); //acertou uma embarcacao;
+            }else if(tabuleiro[i][j]==0){
+                printf("\t*"); //nao acertou nenhuma embarcacao;
+            }
+        }
+        printf("\n");
+    }
+}
